78918d5b59c851b06f5f8be7a7be9a1939c443cf
Alexis <alexis.tumino@gmail.com>
2025-06-10 16:16:24 -0300

    corregir puerto QR

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const express = require("express");
 const apiRoutes = require("./src/routes/routes");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3005;
 
 const corsOptions = {
   origin: "*",
